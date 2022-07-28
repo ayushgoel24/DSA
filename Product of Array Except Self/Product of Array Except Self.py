8088b7b528cccbf23cbd374ddd33c78b1892cd47
Ayush Goel <ayush.goel2427@gmail.com>
2022-07-28 11:40:50 +0530

    python soln

---
+++ Product of Array Except Self/Product of Array Except Self.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answers = [1] * len(nums)
+        
+        temp = 1;
+        for i in range(len(nums)):
+            answers[i] = temp
+            temp *= nums[i]
+        
+        temp = 1
+        for i in range(len(nums) -1, -1, -1):
+            answers[i] *= temp
+            temp *= nums[i]
+        
+        return answers
