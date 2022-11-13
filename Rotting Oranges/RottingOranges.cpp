c9a47f9c9abe658c41207a0370cbb843fc2040e4
ayushgoel24 <ayush.goel2427@gmail.com>
2022-11-13 15:14:33 -0500

    updated c++ code

--- Rotting Oranges/RottingOranges.cpp
+++ Rotting Oranges/RottingOranges.cpp
@@ -1,22 +1,21 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 class Solution {
     
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         vector<vector<int>> gridCopy = grid;
         vector<vector<bool>> vis(grid.size(), vector<bool>(grid[0].size(), false));
-        int deltaRow[] = {-1, 0, 1, 0};
-        int deltaCol[] = {0, 1, 0, -1};
+        int deltaDirection[] = {-1, 0, 1, 0, -1};
 
         queue<pair<pair<int,int>, int>> cont;
+        int fresh_oranges = 0;
 
         for (int row = 0; row < grid.size(); row++) {
             for (int col = 0; col < grid[0].size(); col++) {
-                if (!vis[row][col] && gridCopy[row][col] == 2) {
+                if (gridCopy[row][col] == 2) {
                     cont.push({{row, col}, 0});
                     vis[row][col] = true;
-                }
+                } else if (gridCopy[row][col] == 1) fresh_oranges++;
             }
         }
 
@@ -27,22 +26,20 @@ public:
             int t = cont.front().second;
             cont.pop();
             for (int i=0; i<4; i++) {
-                int nRow = row + deltaRow[i];
-                int nCol = col + deltaCol[i];
+                int nRow = row + deltaDirection[i];
+                int nCol = col + deltaDirection[i+1];
                 if (nRow >= 0 && nRow < gridCopy.size() && nCol >= 0 && nCol < gridCopy[0].size() && !vis[nRow][nCol] && gridCopy[nRow][nCol] == 1) {
                     vis[nRow][nCol] = true;
                     gridCopy[nRow][nCol] = 2;
                     cont.push({{nRow, nCol}, t+1});
                     max_t = max(max_t, t+1);
+                    fresh_oranges--;
                 }
             } 
         }
         
-        for (int row = 0; row < grid.size(); row++) {
-            for (int col = 0; col < grid[0].size(); col++) {
-                if (gridCopy[row][col] == 1) return -1;
-            }
-        }
+        if (fresh_oranges > 0) return -1;
+        
         return max_t;
     }
 };
